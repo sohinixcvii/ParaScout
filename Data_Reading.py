b2697ea8b409d3f2dd6403282a4990ebba7638c8
Julius <jlsmthw@gmail.com>
2026-06-26 13:44:58 +0200

    Julius: Updated data reading with dictionary shaping

--- Data_Reading.py
+++ Data_Reading.py
@@ -1,14 +1,29 @@
 import numpy as np
+import json
 
 
-def data_file_reading():
-    data = np.genfromtxt("5D.txt", names=True)
+data_file = "5D.txt"
+def data_file_reading(data_file):
+    data = np.genfromtxt(data_file, names=True, dtype=None, encoding=None)
     row = len(data)
-    column = len(data[0])
+    column = len(data.dtype.names)
+
+    data_dictionary_shape = {}
+
+    for column_label in data.dtype.names:
+        array = data[column_label]
+        data_list = array.tolist()
+        data_dictionary_shape[column_label] = data_list
+
 
     if column <= 5:
         print(f"Successfully uploaded {column} column(s) and {row} row(s)")
     else:
         print(f"ERROR: The data uploaded has more than 5 columns")
 
-data_file_reading()
+
+    return data_dictionary_shape
+
+# print(json.dumps(data_file_reading(data_file))) # Strictly OPTIONAL for visualizing data.
+
+data_file_reading(data_file)
